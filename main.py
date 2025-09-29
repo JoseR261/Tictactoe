11a26c475850f8ac32b615bfacb1e2367fc989bc
Quentin Mortier <=>
2025-09-29 11:14:44 +0100

    Baseline

---
+++ main.py
@@ -0,0 +1,18 @@
+PLAYER_1 = "X"
+PLAYER_2 = "O"
+EMPTY = " "
+
+def who_wins(game: list[str]) -> str:
+    pass
+
+def game_is_full(game: list[str]) -> bool:
+    pass
+
+def player_move(game: list[str], player: str) -> list[str]:
+    pass
+
+def print_game(game: list[str]) -> None:
+    pass
+
+def main() -> None:
+    pass
