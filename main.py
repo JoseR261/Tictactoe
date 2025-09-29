53df26e9cf360d4d4f83224328ee5c6543fce8d3
JoseR261 <joseroodriguess@gmail.com>
2025-09-29 12:19:04 +0100

    Base function

--- main.py
+++ main.py
@@ -16,3 +16,54 @@ def print_game(game: list[str]) -> None:
 
 def main() -> None:
     pass
+
+def game_is_full(game: list[str]):
+    for cell in game:
+        if cell == EMPTY:
+            return False
+    return True
+
+def who_wins(game: list[str]) -> str:
+    if game[0]==game[1]==game[2] and game != EMPTY:
+        return game[0]
+    if game[0]==game[3]==game[6] and game != EMPTY:
+        return game[0]
+    if game[0]==game[4]==game[8] and game != EMPTY:
+        return game[0]
+    if game[1]==game[4]==game[7] and game != EMPTY:
+        return game[1]
+    if game[2]==game[5]==game[8] and game != EMPTY:
+        return game[2]
+    if game[2]==game[4]==game[6] and game != EMPTY:
+        return game[2]
+    if game[6]==game[7]==game[8] and game != EMPTY:
+        return game[6]
+    if game[3]==game[4]==game[5] and game != EMPTY:
+        return game[3]
+
+    return EMPTY
+
+
+
+
+
+
+
+
+
+
+
+
+ if who_wins(game) == 'x':
+     end_game = True
+     print_game(who_wins('Player_1'))
+
+ while who_wins(game) == 'o':
+     end_game = True
+     print_game(who_wins('Player_2'))
+
+if game_is_full:
+     end_game = True
+     print_game(who_wins(game))
+
+
